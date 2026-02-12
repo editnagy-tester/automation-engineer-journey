4aba60a2d7d41d975c131d98150758a0cc65c76b
Edit Nagy <nagyedit.w@gmail.com>
2026-02-12 21:51:57 +0100

    Add TaskItem class with properties and constructor

---
+++ TaskItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager
+{
+    internal class TaskItem
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        bool IsCompleted { get; set; } = false;
+
+
+
+        public TaskItem(int id, string title)
+        {
+            Id = id;
+            Title= title;
+        }
+    }
+}
